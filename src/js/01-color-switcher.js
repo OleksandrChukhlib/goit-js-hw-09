69ec379fbdbe27c63a14d9daa4f43b296c78d1a6
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2022-02-04 08:57:26 +0200

    Finished task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startColorChangeBtnRef = document.querySelector('[data-start]');
+const stopColorChangeBtnRef = document.querySelector('[data-stop]');
 
+startColorChangeBtnRef.classList.add('color-switcher__start');
+stopColorChangeBtnRef.classList.add('color-switcher__stop');
+
+startColorChangeBtnRef.addEventListener('click', onStartColorChangeBtn);
+stopColorChangeBtnRef.addEventListener('click', onStopColorChangeBtn);
+
+const DELAY = 1000;
+let timeoutId = null;
+
+function onStartColorChangeBtn() {
+  timeoutId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+
+  startColorChangeBtnRef.setAttribute('disabled', true);
+  stopColorChangeBtnRef.removeAttribute('disabled');
+}
+
+function onStopColorChangeBtn() {
+  clearInterval(timeoutId);
+  stopColorChangeBtnRef.setAttribute('disabled', true);
+  startColorChangeBtnRef.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
